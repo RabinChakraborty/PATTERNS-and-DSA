985e7a3f237154d7d0863b6f05d9d39618e998ce
Rabin Chakraborty <rabin747809@gmail.com>
2023-06-21 04:21:03 +0530

    Addidng the frist two patern solutions

---
+++ Pattern no.1.java
@@ -0,0 +1,20 @@
+// * * *
+// * * *
+// * * *
+
+class Solution {
+
+    void printSquare() {
+        // code here
+        int n=5;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            System.out.print("* ");
+        }
+        System.out.println("");
+    }
+        
+    }
+
+    
+}
